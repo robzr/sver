3dcf846f8dcbb8526f5d652fda2292df5f7e0c5f
Rob Zwissler <rob.zwissler@lovevery.com>
2024-02-22 15:08:04 -0800

    update homebrew formula

--- homebrew/sver.rb
+++ homebrew/sver.rb
@@ -2,7 +2,7 @@ class Sver < Formula
   desc "Semver (Semantic Version) parsing & utility script and function library in pure bash"
   homepage "https://github.com/robzr/sver"
   url "https://github.com/robzr/sver/releases/download/v1.0.0/sver"
-  sha256 "1f1c3b26e980f8b89d2c13030a63074b6af6784c067cd0e7f612d247d2384fa5"
+  sha256 "a5571a9bac577819943fb72c23ce7a9989f8e61b39f68522b5625dba4586b77a"
   license "CC-BY-SA-3.0"
 
   def install
